28bd2bd22ba316a7478b00f14080b6c9c1e5f351
Gian Sanjaya <mortarsanjaya@gmail.com>
2020-03-20 23:28:19 -0400

    Update main.cpp

--- CoinWeight/main.cpp
+++ CoinWeight/main.cpp
@@ -22,31 +22,8 @@ int main() {
     gameView = make_unique<GameViewX11>();
     gameModel->updateView(*gameView);
     while (true) {
-        std::string s;
-        cin >> s;
-        if (cin.fail()) break;
-        Input inp(s[0]);
-        if (s[0] == 'n') {
-            inp = Input('\n');
-        } else if (s[0] == 'u') {
-            inp = Input(Input::Arrow::Up);
-        } else if (s[0] == 'j') {
-            inp = Input(Input::Arrow::Down);
-        } else if (s[0] == 'h') {
-            inp = Input(Input::Arrow::Left);
-        } else if (s[0] == 'k') {
-            inp = Input(Input::Arrow::Right);
-        }
-        gameModel->processInput(inp);
+        gameView->receiveInput();
+        gameModel->processInput(gameView->lastInput());
         gameModel->updateView(*gameView);
     }
 }
-
-/*
-    README
-    Game Model components pretty much done
-    The Game Model itself is far from done, I'd say
-    Game View is also quite far from done
-    However, looking at the design (and the fact that we're not
-        time-pressured, this is not the biggest issue)
-*/
