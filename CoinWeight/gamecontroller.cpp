7d71586c2060fb22d11c0e0173d725b5a0f30acf
Gian Sanjaya <mortarsanjaya@gmail.com>
2020-03-29 04:33:43 -0400

    Added empty controller class

---
+++ CoinWeight/gamecontroller.cpp
@@ -0,0 +1,9 @@
+//
+//  gamecontroller.cpp
+//  CoinWeight
+//
+//  Created by Gian Cordana Sanjaya on 2020-03-29.
+//  Copyright © 2020 -. All rights reserved.
+//
+
+#include "gamecontroller.hpp"
---
+++ CoinWeight/gamecontroller.hpp
@@ -0,0 +1,17 @@
+//
+//  gamecontroller.hpp
+//  CoinWeight
+//
+//  Created by Gian Cordana Sanjaya on 2020-03-29.
+//  Copyright © 2020 -. All rights reserved.
+//
+
+#ifndef gamecontroller_hpp
+#define gamecontroller_hpp
+
+class GameController {
+public:
+    
+};
+
+#endif
