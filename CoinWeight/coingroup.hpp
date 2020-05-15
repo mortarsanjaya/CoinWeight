9db17e73b5dbfc46be686287bc8bcc466069553d
Gian Sanjaya <mortarsanjaya@gmail.com>
2020-05-15 17:47:30 -0400

    Create coingroup.hpp

---
+++ CoinWeight/coingroup.hpp
@@ -0,0 +1,19 @@
+//
+//  coingroup.hpp
+//  CoinWeight
+//
+//  Created by Gian Cordana Sanjaya on 2020-05-15.
+//  Copyright © 2020 -. All rights reserved.
+//
+
+#ifndef coingroup_h
+#define coingroup_h
+
+enum class CoinGroup {
+    NoSelect,       // Not selected
+    LeftWeigh,      // Put to left scale
+    RightWeigh,     // Put to right scale
+    Guess           // Guess as fake coin
+};
+
+#endif
