7d7f08a898a39a8626d6eb454d83756938429917
Gian Sanjaya <mortarsanjaya@gmail.com>
2020-05-18 15:47:12 -0400

    small change.

--- CoinWeight/gamescreen.hpp
+++ CoinWeight/gamescreen.hpp
@@ -19,20 +19,20 @@ public:
     virtual ~GameScreen() = default;
 
     // Action on arrow button press
-    // Generally, does not change the view itself other than the screen
-    virtual void highlightUp(GameController &view) = 0;
-    virtual void highlightDown(GameController &view) = 0;
-    virtual void highlightLeft(GameController &view) = 0;
-    virtual void highlightRight(GameController &view) = 0;
+    // Generally, does not change the controller itself other than the screen
+    virtual void highlightUp(GameController &controller) = 0;
+    virtual void highlightDown(GameController &controller) = 0;
+    virtual void highlightLeft(GameController &controller) = 0;
+    virtual void highlightRight(GameController &controller) = 0;
     
     // Action on character input press
-    virtual void onCharInput(GameController &view, const char inputChar) = 0;
+    virtual void onCharInput(GameController &controller, const char inputChar) = 0;
     
     // Action on return button press (most certainly screen switching)
-    virtual void onReturnButton(GameController &view) = 0;
+    virtual void onReturnButton(GameController &controller) = 0;
     
     // Triggers View and UI for displaying the current screen
-    virtual void triggerDisplay(GameController &view, GameUI &interface) = 0;
+    virtual void triggerDisplay(GameController &controller, GameUI &interface) = 0;
     
 };
 
