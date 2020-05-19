f09fd495e4a1b6a2996ea2724d755b0b9581e11d
Gian Sanjaya <mortarsanjaya@gmail.com>
2020-05-19 17:20:29 -0400

    Small change on Coin Set interface.

--- CoinWeight/coinset.hpp
+++ CoinWeight/coinset.hpp
@@ -16,25 +16,24 @@
 enum class GameLevel;
 
 class CoinSet final {
-    size_t nCoins;
-    size_t fakeCoinI1;
-    size_t fakeCoinI2;
-    
-	static constexpr size_t numOfFakeCoins = 2;
-    const bool isFakeCoinIndex(const size_t index) const;
-
 public:
 	CoinSet(size_t numOfCoins);
     
 	const size_t numOfCoins() const;
-    const size_t fakeCoinIndex1() const;
-    const size_t fakeCoinIndex2() const;
 	
 	// Game operations
 	const WeighResult compareWeight(const CoinSelection &weighing) const;
 	const GuessResult guessFakeCoins(const CoinSelection &guess) const;
  
     friend void computerTest(const size_t nCoinsCap, const GameLevel level);
+    
+private:
+    size_t nCoins;
+    size_t fakeCoinI1;
+    size_t fakeCoinI2;
+    
+	static constexpr size_t numOfFakeCoins = 2;
+    const bool isFakeCoinIndex(const size_t index) const;
 };
 
 #endif
