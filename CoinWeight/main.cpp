e2e2b4fe0ae0cb3fdee63362b5894eaea7a7bc30
Gian Sanjaya <mortarsanjaya@gmail.com>
2020-06-11 07:36:28 -0400

    Update main.cpp

--- CoinWeight/main.cpp
+++ CoinWeight/main.cpp
@@ -9,24 +9,29 @@
 #include "coinset.hpp"
 #include "computertest.hpp"
 #include "controller.hpp"
-#include "viewx11.hpp"
 
+#include <iostream>
 #include <chrono>
 #include <thread>
 
 using namespace CoinWeight;
+using namespace std::chrono;
 
 int main() {
     computerTest(75, GameLevel::Easy);
     computerTest(100, GameLevel::Medium);
     computerTest(100, GameLevel::Hard);
 
-/*
-    auto controller = std::make_unique<Controller>();
+    Controller controller;
     while (true) {
-        controller->updateDisplay();
-        std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        controller->processInput();
+        controller.updateDisplay();
+        
+        auto before_input = steady_clock::now();
+        controller.processInput();
+        while (true) {
+            auto after_input = steady_clock::now();
+            auto duration = duration_cast<milliseconds>(after_input - before_input);
+            if (duration.count() >= 10) break;
+        }
     }
-*/
 }
